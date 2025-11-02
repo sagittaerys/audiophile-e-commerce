381fbb6838bb3a8386c84fefb367f224b7e2434c
Olamilekan Aremu <aremu0235@gmail.com>
2025-11-02 13:26:53 +0100

    feat:responsiveness i

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ export default function Home() {
     <div className="container text-[#FFFFFF] ">
       
       <section>
-        <div className="flex px-20 bg-[#0E0E0E] text-[#FFFFFF] h-[729px] items-center justify-between gap-4 mx-auto">
-          <div className="md:max-w-[398px] h-[346px] flex flex-col justify-between">
+        <div className="flex lg:px-20 bg-[#0E0E0E] text-[#FFFFFF] lg:h-[729px] lg:items-center lg:justify-between lg:gap-4 lg:mx-auto">
+
+          <div className="lg::max-w-[398px] lg:h-[346px] flex flex-col justify-between">
             <h3 className="text-[14px] tracking-[10px] font-normal ">
               NEW PRODUCT
             </h3>
             <div>
-              <h1 className="text-[56px] tracing-[2px] font-bold leading-[58px] ">
+              <h1 className="lg:text-[56px] tracing-[2px] font-bold leading-[58px] ">
                 XX99 Mark II Headphones
               </h1>
             </div>
             <div>
-              <p className="text-[15px] leading-[25px]">
+              <p className="lg:text-[15px] leading-[25px]">
                 Experience natural, lifelike audio and exceptional build quality
                 made for the passionate music enthusiast.
               </p>
             </div>
 
-            <button className="w-[160px] h-[48px] text-[13px] tracking-[1px] font-bold bg-[#D87D4A]">
+            <button className="lg:w-[160px] lg:h-[48px] text-[13px] tracking-[1px] font-bold bg-[#D87D4A]">
               SEE PRODUCT
             </button>
           </div>
@@ -40,15 +41,15 @@ export default function Home() {
 
       {/* product card */}
 
-      <div className="px-20">
-        <div className="mt-70">
+      <div className="lg:px-20">
+        <div className="lg:mt-70">
           <ProductList />
         </div>
 
         {/* speaker section */}
 
-        <div className="mt-40  h-[560px] bg-[#D87D4A] rounded-[8px]  relative flex items-center justify-center">
-          <div className="absolute -left-[30px] -top-[20px]">
+        <div className="lg:mt-40  lg:h-[560px] bg-[#D87D4A] rounded-[8px]  relative flex items-center justify-center">
+          <div className="absolute lg:-left-[30px] lg:-top-[20px]">
             <Image
               width={944}
               height={944}
@@ -57,7 +58,7 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex justify-between h-full  gap-20 items-center">
+          <div className="flex justify-between h-full  lg:gap-20 items-center">
             <div className="flex mx-auto flex-col justify-end  h-full">
               <Image
                 src="/big-speaker.png"
@@ -69,8 +70,8 @@ export default function Home() {
             </div>
 
             {/* Content Box */}
-            <div className="flex flex-col justify-between  bg-transparent w-[349px] h-[303px]  ">
-              <h1 className="text-white md:text-[56px]  font-bold  leading-[58px] tracking-[2px] ">
+            <div className="flex flex-col justify-between  bg-transparent lg:w-[349px] h-[303px]  ">
+              <h1 className="text-white lg:text-[56px]  font-bold  leading-[58px] tracking-[2px] ">
                 ZX9
                 <br />
                 SPEAKER
@@ -81,7 +82,7 @@ export default function Home() {
                 deliver truly remarkable sound.
               </p>
 
-              <button className="bg-[#000000] w-[160px] h-[48px] text-[13px] tracking-[1px]  hover:bg-gray-800 transition-colors duration-300">
+              <button className="bg-[#000000] lg:w-[160px] lg:h-[48px] text-[13px] tracking-[1px]  hover:bg-gray-800 transition-colors duration-300">
                 SEE PRODUCT
               </button>
             </div>
@@ -89,29 +90,29 @@ export default function Home() {
         </div>
 
         {/* next */}
-        <div className="h-[320px] my-10 bg-[url('/next-speaker.png')] bg-cover bg-center flex flex-col justify-center  w-full  px-30 rounded-[8px]">
-          <div className="w-[204px] h-[118px]   justify-between flex flex-col justify-between">
+        <div className="lg:h-[320px] lg:my-10 bg-[url('/next-speaker.png')] bg-cover bg-center flex flex-col justify-center  w-full  lg:px-30 rounded-[8px]">
+          <div className="lg:w-[204px] lg:h-[118px]   justify-between flex flex-col justify-between">
             <h2 className="text-[28px] text-[#000000] tracking-[2px] font-bold">
               ZX7 SPEAKER
             </h2>
 
-            <button className="bg-transparent border border-[#000000] w-[160px] h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white  transition-colors duration-300">
+            <button className="bg-transparent border border-[#000000] lg:w-[160px] lg:h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white  transition-colors duration-300">
               SEE PRODUCT
             </button>
           </div>
         </div>
 
         {/* next */}
-        <div className="h-[340px] flex justify-between  ">
-          <div className="w-[540px] rounded-[8px] bg-[url('/pods.png')] bg-cover bg-center"></div>
+        <div className="lg:h-[340px] flex justify-between  ">
+          <div className="lg:w-[540px] rounded-[8px] bg-[url('/pods.png')] bg-cover bg-center"></div>
 
-          <div className="w-[540px] rounded-[8px] flex flex-col justify-center px-30 bg-[#F1F1F1]">
-            <div className="w-[247px] h-[118px] justify-between flex flex-col ">
-              <h2 className="text-[24px] text-[#000000] tracking-[2px] font-bold">
+          <div className="lg:w-[540px] rounded-[8px] flex flex-col justify-center px-30 bg-[#F1F1F1]">
+            <div className="lg:w-[247px] lg:h-[118px] justify-between flex flex-col ">
+              <h2 className="lg:text-[24px] text-[#000000] tracking-[2px] font-bold">
                 YX1 EARPHONES
               </h2>
 
-              <button className="bg-transparent border border-[#000000] w-[160px] h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white  transition-colors duration-300">
+              <button className="bg-transparent border border-[#000000] lg:w-[160px] lg:h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white  transition-colors duration-300">
                 SEE PRODUCT
               </button>
             </div>
@@ -119,8 +120,8 @@ export default function Home() {
         </div>
 
         {/* next */}
-        <div className="my-40 h-[588px] flex justify-between items-center">
-          <div className="max-w-[445px] h-[295px] flex flex-col justify-between">
+        <div className="lg:my-40 lg:h-[588px] flex justify-between items-center">
+          <div className="lg:max-w-[445px] lg:h-[295px] flex flex-col justify-between">
             <h2 className="text-[40px] text-[#000000] leading-[44px] tracking-[1.43px] font-bold ">
               BRINGING YOU THE <span className="text-[#D87D4A]">BEST </span>{" "}
               AUDIO GEAR
