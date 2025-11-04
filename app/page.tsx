372b225d0f03948b3ce7d86b108efbd3add1eb61
Olamilekan Aremu <aremu0235@gmail.com>
2025-11-04 01:28:14 +0100

    feat:god abeg

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         {/* speaker section */}
 
         <div
-          className="lg:mt-40 mx-auto lg:h-[560px] h-[600px] w-[327px] mb-10 md:h-[720px] md:w-[689px] lg:w-[100%] bg-[#D87D4A] rounded-[8px] lg:relative lg:flex lg:items-center lg:justify-center
+          className="lg:mt-40 mx-auto lg:h-[560px] h-[600px] pt-10 w-[327px] mb-10 md:h-[720px] md:w-[689px] lg:w-[100%] bg-[#D87D4A] rounded-[8px] lg:relative lg:flex lg:items-center lg:justify-center
         
         md:flex md:flex-col md:items-center md:justify-center md:relative md:overflow-hidden
         "
@@ -71,19 +71,17 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col  h-[100%]  lg:flex-row md:flex-col justify-between md:h-full  lg:gap-20 md:gap-3 md:py-10 lg:py-0 lg:items-end">
-
             <div className="flex flex-col  mx-auto lg:h-full md:h-auto  lg:justify-end md:justify-center">
-
               <Image
                 src="/big-speaker.png"
                 alt="ZX9 Speaker"
                 width={410.23}
                 height={493}
-                className="drop-shadow-2xl md:w-[197px] w-[172.25px] h-[207px] md:h-[237px] lg:w-[410.23px]"
+                className="drop-shadow-2xl md:w-[197px] w-[172.25px] h-[207px] md:h-[237px] lg:w-[410.23px] lg:h-[493px]"
               />
             </div>
 
-            <div className="flex flex-col mt-2 md:mx-auto  justify-between md:text-center h-full py-10 bg-transparent lg:w-[349px] lg:h-[303px] h-auto text-center lg:text-left px-8 lg:mb-16">
+            <div className="flex flex-col mt-2 md:mx-auto space-y-3 pt-10 md:justify-between md:text-center h-full  bg-transparent lg:w-[349px] lg:h-[303px] h-auto text-center lg:text-left px-8 lg:mb-16">
               <h1 className="text-white lg:text-[56px] w-[280px] text-[36px] md:text-[48px] md:mx-auto font-bold leading-[58px] tracking-[2px]">
                 ZX9
                 <br />
@@ -103,7 +101,8 @@ export default function Home() {
         </div>
 
         {/* next */}
-        <div className="lg:h-[320px] w-[327px] my-10 mx-auto h-[320px] md:px-10 md:w-[689px] md:h-[320px] md:my-10 lg:my-10 bg-[url('/next-speaker.png')] bg-cover bg-center flex flex-col justify-center  lg:w-full  lg:px-30 rounded-[8px]">
+
+        <div className="lg:h-[320px] w-[327px] my-10 mx-auto h-[320px] md:px-10 md:w-[689px] md:h-[320px] md:my-10 lg:my-10 bg-[url('/next-speaker.png')] bg-cover bg-center flex flex-col justify-center  lg:w-full  lg:px-30 rounded-[8px] ">
           <div className="lg:w-[204px] w-[204px] h-[118px]  lg:h-[118px] pl-5  justify-between flex flex-col justify-between">
             <h2 className="text-[23px] text-[#000000] tracking-[2px] font-bold">
               ZX7 SPEAKER
@@ -116,16 +115,16 @@ export default function Home() {
         </div>
 
         {/* next */}
-        <div className="lg:h-[340px] md:h-[320px] md:my-10 flex justify-between ">
-          <div className="lg:w-[540px]  md:w-[339px] rounded-[8px] bg-[url('/pods.png')] bg-cover bg-center"></div>
+        <div className="h-[424px] flex-col gap-6 items-center md:items-stretch md:flex-row md:h-[320px] md:w-[689px] md:my-10 lg:h-[340px] lg:w-[100%] flex justify-between ">
+          <div className="w-[327px] h-[200px] md:h-auto md:w-[339px] lg:w-[540px] rounded-[8px] bg-[url('/pods.png')] bg-cover bg-center"></div>
 
-          <div className="lg:w-[540px] md:w-[339px] rounded-[8px] flex flex-col justify-center  md:px-13 lg:px-30 bg-[#F1F1F1] ">
-            <div className="w-[247px] h-[118px] justify-between flex  flex-col">
+          <div className="w-[327px] h-[200px] md:h-auto md:w-[339px] lg:w-[540px] rounded-[8px] flex flex-col justify-center px-6 md:px-13 lg:px-30 bg-[#F1F1F1]">
+            <div className="w-[247px] h-[118px] justify-between flex flex-col">
               <h2 className="text-[24px] text-[#000000] tracking-[2px] font-bold">
                 YX1 EARPHONES
               </h2>
 
-              <button className="bg-transparent border border-[#000000] w-[160px] h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white  transition-colors duration-300">
+              <button className="bg-transparent border border-[#000000] w-[160px] h-[48px] text-[13px] tracking-[1px] hover:bg-[#000000] font-bold text-[#000000] hover:text-white transition-colors duration-300">
                 SEE PRODUCT
               </button>
             </div>
@@ -133,14 +132,14 @@ export default function Home() {
         </div>
 
         {/* next */}
-        <div className="lg:my-40 lg:h-[588px] md:my-10 lg:w-[1110px] md:w-[689px] lg:flex lg:justify-between lg:flex-row lg:items-center md:flex-col-reverse md:flex md:gap-12">
-          <div className="lg:w-[445px] md:w-[573px]  md:h-[270px] lg:text-left md:text-center md:mx-auto lg:h-[295px] lg:flex lg:flex-col lg:justify-between">
-            <h2 className="text-[40px] text-[#000000] leading-[44px] tracking-[1.43px] font-bold ">
-              BRINGING YOU THE <span className="text-[#D87D4A]">BEST </span>{" "}
+        <div className="my-16 h-auto flex flex-col-reverse gap-10 items-center md:items-stretch md:my-10 md:flex md:flex-col-reverse md:gap-12 md:w-[689px] lg:my-40 lg:h-[588px] lg:w-[1110px] lg:flex lg:justify-between lg:flex-row lg:items-center">
+          <div className="w-[327px] text-center px-6 md:w-[573px] md:h-[270px] md:text-center md:mx-auto lg:w-[445px] lg:h-[295px] lg:flex lg:flex-col lg:justify-between lg:text-left lg:px-0">
+            <h2 className="text-[28px] mb-8 md:mb-0 md:text-[40px] text-[#000000] leading-[38px] md:leading-[44px] tracking-[1px] md:tracking-[1.43px] font-bold">
+              BRINGING YOU THE <span className="text-[#D87D4A]">BEST</span>{" "}
               AUDIO GEAR
             </h2>
 
-            <p className="text-[#000000] text-[15px] md:h-[150px]  leading-[25px]">
+            <p className="text-[#000000] text-[15px] leading-[25px] opacity-50 md:opacity-100 md:h-[150px]">
               Located at the heart of New York City, Audiophile is the premier
               store for high end headphones, earphones, speakers, and audio
               accessories. We have a large showroom and luxury demonstration
@@ -151,7 +150,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="rounded-[8px] md:w-[689px] md:h-[300px] lg:w-[540px] lg:h-[588px] bg-[url('/user.png')] bg-cover bg-center"></div>
+          <div className="rounded-[8px] w-[327px] h-[300px] md:w-[689px] md:h-[300px] lg:w-[540px] lg:h-[588px] bg-[url('/user.png')] bg-cover bg-center"></div>
         </div>
       </div>
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,10 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="w-full overflow-hidden flex flex-col justify-between lg:h-[365px] md:h-[400px] md:w-[768px] lg:w-[100%] container md:px-10 lg:px-20 text-[#FFFFFF] py-10 bg-[#101010] ">
+    <footer className="w-full overflow-hidden flex flex-col justify-between h-auto py-12 px-6 md:py-10 md:px-10 md:h-[400px] md:w-[768px] lg:h-[365px] lg:w-[100%] lg:px-20 container text-[#FFFFFF] bg-[#101010]">
 
-      <div className="lg:flex lg:flex-row md:flex-col  md:py-4 md:gap-4  lg:items-center lg:justify-between lg:py-8">
+      {/* Logo and Navigation */}
+      <div className="flex flex-col items-center gap-12 md:items-start md:gap-8 lg:flex-row lg:items-center lg:justify-between lg:py-8 md:flex-col md:py-4 md:gap-4">
         <Image
           src="/audiophile-logo.png"
           alt="Brand Logo"
@@ -16,7 +17,7 @@ export default function Footer() {
           height={25}
         />
 
-        <ul className="flex tracking-[2px] md:w-[429px] text-[13px] flex-row md:gap-6 md:py-4 lg:gap-8 font-bold">
+        <ul className="flex flex-col items-center gap-4 md:flex-row tracking-[2px] md:w-[429px] text-[13px] md:gap-6 md:py-4 lg:gap-8 font-bold">
           <Link href="/home">
             <li className="hover:text-[#D87D4A] transition-colors duration-300">
               HOME
@@ -41,18 +42,19 @@ export default function Footer() {
         </ul>
       </div>
 
-      <div className=" flex  lg:justify-between lg:flex-row gap-10 md:flex-col lg:w-[100%] h-[100px]">
+      {/* Description and Social Icons (Desktop) */}
+      <div className="flex flex-col my-10 gap-12 md:gap-10 lg:justify-between lg:flex-row md:flex-col lg:w-[100%] h-auto md:h-[100px]">
 
-        <p className="lg:w-[50%] md:w-full text-[#FFFFFF]/50 text-[15px] leading-[25px]">
+        <p className="text-center md:text-left lg:w-[50%] md:w-full text-[#FFFFFF]/50 text-[15px] leading-[25px]">
           Audiophile is an all in one stop to fulfill your audio needs. We're a
           small team of music lovers and sound specialists who are devoted to
           helping you get the most out of personal audio. Come and visit our
-          demo facility - we’re open 7 days a week.
+          demo facility - we're open 7 days a week.
         </p>
 
-     
-        <div className="md:hidden lg:flex lg:flex-col lg:w-[50%] lg:justify-end">
-          <div className="lg:flex   lg:items-center gap-5 lg:justify-end  ">
+       
+        <div className="md:hidden hidden lg:flex lg:flex-col lg:w-[50%] lg:justify-end">
+          <div className="lg:flex lg:items-center gap-5 lg:justify-end">
             <Link
               className="hover:text-[#D87D4A] text-white transition-colors duration-300"
               href="http://www.github.com/sagittaerys"
@@ -77,14 +79,15 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="flex items-center   justify-between">
-        <p className="text-[#FFFFFF]/50  md:w-[689px] text-[15px] lg:w-[540px] leading-[25px]">
+      {/* Copyright and Social Icons (Mobile/Tablet) */}
+      <div className="flex flex-col items-center gap-12 md:flex-row md:items-center md:justify-between md:gap-0">
+        <p className="text-[#FFFFFF]/50 text-center md:text-left md:w-[689px] text-[15px] lg:w-[540px] leading-[25px]">
           Copyright 2021. All Rights Reserved
         </p>
 
-
-        <div className="lg:hidden flex flex-col justify-end">
-          <div className="flex items-center gap-5 justify-end w-[540px] ">
+        {/* Social Icons - Mobile/Tablet Only */}
+        <div className="flex justify-center md:justify-end lg:hidden">
+          <div className="flex items-center gap-5">
             <Link
               className="hover:text-[#D87D4A] text-white transition-colors duration-300"
               href="http://www.github.com/sagittaerys"
@@ -108,8 +111,6 @@ export default function Footer() {
           </div>
         </div>
       </div>
-
-     
     </footer>
   );
 }
